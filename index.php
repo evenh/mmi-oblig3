c1eeaf14cc0222b59bcf24cb3db38ebd743a7034
Vegar Norman <vegar@theinternetloves.me>
2013-11-27 10:36:01 +0100

    Endringer i struktur
    - Fått inn noen wrapper-elementer
    - Self-closing tags *kremt*
    - Javascript i bunnen av body
    - Bedre viewport for applikasjoner
    - Tatt ut Bootstrap

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <!doctype html>
 <html lang="no">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic" />
+	<link rel="stylesheet" type="text/css" href="css/mmi.css" />
+
 	<title>Applikasjonstittel 1.0</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
-	<link rel="stylesheet" href="css/mmi.css" media="screen">
-	<!-- JS -->
-	<script src="js/jquery-1.10.2.min.js"></script>
-	<script src="js/app.js"></script>
 </head>
 <body>
+
 	<div class="container">
-	<h1>Applikasjonstittel</h1>
-	<p id="message">Laster inn applikasjonen...</p>
-	<h2>Historikk</h2>
-	<ol id="historikk">
-	</ol>
-</div>
+
+		<header>
+			<h1>Applikasjonstittel</h1>
+		</header>
+
+		<section>
+			<p id="message">Laster inn applikasjonen...</p>
+			<h2>Historikk</h2>
+
+			<ol id="historikk">
+			</ol>
+		</section>
+	</div>
+
+<!-- Javascript -->
+
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/app.js"></script>
+
 </body>
 </html>
