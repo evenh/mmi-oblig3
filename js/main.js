11003ab453cfe0add98fc2b7d217f24261457e62
Vegar Norman <vegar@theinternetloves.me>
2013-12-02 21:30:03 +0100

    Tatt ut fullskjermmodus og splash

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
 $(function() {
-
-	$("body").prepend('<p id="splash">Velkommen til <span>Sign.al</span>. Trykk på skjermen for å begynne.</p>');
-
-	$("#splash").click(function() {
-		launchFullScreen(document.documentElement);
-		$(this).remove();
-	});
-
   $("a").click(function (e){ e.preventDefault(); window.location = $(this).attr("href"); });
 });
-
-function launchFullScreen(element) {
-  if(element.requestFullScreen) {
-    element.requestFullScreen();
-  } else if(element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if(element.webkitRequestFullScreen) {
-    element.webkitRequestFullScreen();
-  }
-}
